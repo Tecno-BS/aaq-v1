73d700d475c62d551e7c6b9c96ee8139da2800bf
unknown <administrador@BRANDSTRATX.SAS.CORP>
2026-03-25 21:22:23 -0500

    added cors

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,11 @@ app = FastAPI(title="AAQ-BS", version="0.1.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://192.168.2.4:5173",  # Acceso por IP de red local
+    ],
     allow_credentials = True,
     allow_methods=["*"],
     allow_headers=["*"],
